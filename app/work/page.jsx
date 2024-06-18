59484eddcf1c93e20b6a74319099c385148adb4d
Evian <115055847+eviannaive@users.noreply.github.com>
2024-06-19 00:27:21 +0800

    work:swiper

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,5 +1,181 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+import WorkSlideBtns from "@/components/layouts/WorkSlideBtns";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, assumenda quam! Corporis consequuntur earum.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/photo1.jpeg",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "frontend",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, assumenda quam! Corporis consequuntur earum.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/photo2.jpeg",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, assumenda quam! Corporis consequuntur earum.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/photo3.jpeg",
+    live: "",
+    github: "",
+  },
+  {
+    num: "04",
+    category: "full stack",
+    title: "project 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, assumenda quam! Corporis consequuntur earum.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/photo4.jpeg",
+    live: "",
+    github: "",
+  },
+];
+
 const Page = () => {
-  return <div>page</div>;
+  const [project, setProject] = useState(projects[0]);
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.realIndex;
+    setProject(projects[currentIndex]);
+  };
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div class="flex flex-col gap-[30px] h-[50%]">
+              {/* outline num*/}
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              {/* project category */}
+              <h2 class="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              {/* project description */}
+              <p class="text-white/60">{project.description}</p>
+              {/* stack */}
+              <ul class="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accont">
+                      {item.name}
+                      {/* remove the last comma */}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              {/* buttons */}
+              <div class="flex items-center gap-4">
+                {/* live project button */}
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipTrigger>
+                        <p>Live project</p>
+                      </TooltipTrigger>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                {/* github project button */}
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipTrigger>
+                        <p>Github repository</p>
+                      </TooltipTrigger>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidePerView={1}
+              loop={true}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      {/* overlay */}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/* image */}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt=""
+                        ></Image>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              {/* slider button */}
+              <WorkSlideBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Page;
